ad5e21192c7b749521ccb30cd7d35d1dbbf02476
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-09-10 21:09:47 +0500

    Add files via upload

---
+++ ДЗ №9 до 18.09.23 Исключения и пространства имен/ДЗ №9 до 18.09.23 Исключения и пространства имен.cpp
@@ -0,0 +1,197 @@
+﻿// ДЗ №9 до 18.09.23 Исключения и пространства имен
+// 
+// Задание 1. Возьмите код очередей из предыдущих домашних заданий и
+// добавьте обработку исключений для случаев, когда происходит попытка
+// достать объект из пустой очереди или добавить объект в полную очередь.
+
+
+#include <iostream>
+using namespace std;
+
+// Определяем базовый класс "Список"
+class List 
+{
+public:
+    virtual void insert(int value) = 0;     // Виртуальная функция для вставки элемента
+    virtual int remove() = 0;               // Виртуальная функция для вытаскивания элемента
+    virtual bool isEmpty() const = 0;      // Виртуальная функция для проверки, пуст ли список
+};
+
+// Определяем производный класс "Стек" на основе базового класса "Список"
+class Stack : public List 
+{
+private:
+    int* data;          // Массив для хранения элементов стека
+    int top;            // Индекс вершины стека
+    int capacity;       // Вместимость стека
+
+public:
+    Stack(int size) : top(-1), capacity(size) 
+    {
+        data = new int[size];
+    }
+
+    ~Stack() 
+    {
+        delete[] data;
+    }
+
+    void insert(int value) override 
+    {
+        try 
+        {
+            if (top < capacity - 1) 
+            {
+                data[++top] = value;
+            }
+            else 
+            {
+                throw "Стек переполнен.";  // Генерируем исключение с сообщением
+            }
+        }
+        catch (const char* e) 
+        {
+            cout << "Произошла ошибка: " << e << endl;
+            throw;  // Повторно генерируем исключение
+        }
+    }
+
+    int remove() override 
+    {
+        try 
+        {
+            if (!isEmpty()) 
+            {
+                return data[top--];
+            }
+            else 
+            {
+                throw "Стек пуст.";  // Генерируем исключение с сообщением
+            }
+        }
+        catch (const char* e) 
+        {
+            cout << "Произошла ошибка: " << e << endl;
+            throw;  // Повторно генерируем исключение
+        }
+    }
+
+    bool isEmpty() const override 
+    {
+        return top == -1;
+    }
+};
+
+// Определяем производный класс "Очередь" на основе базового класса "Список"
+class Queue : public List 
+{
+private:
+    int* data;          // Массив для хранения элементов очереди
+    int front;          // Индекс начала очереди
+    int rear;           // Индекс конца очереди
+    int capacity;       // Вместимость очереди
+
+public:
+    Queue(int size) : front(0), rear(-1), capacity(size) 
+    {
+        data = new int[size];
+    }
+
+    ~Queue() 
+    {
+        delete[] data;
+    }
+
+    void insert(int value) override 
+    {
+        try 
+        {
+            if (rear < capacity - 1) 
+            {
+                data[++rear] = value;
+            }
+            else 
+            {
+                throw "Очередь переполнена.";  // Генерируем исключение с сообщением
+            }
+        }
+        catch (const char* e) 
+        {
+            cout << "Произошла ошибка: " << e << endl;
+            throw;  // Повторно генерируем исключение
+        }
+    }
+
+    int remove() override 
+    {
+        try 
+        {
+            if (!isEmpty()) 
+            {
+                int removedValue = data[front];
+                front++;
+                return removedValue;
+            }
+            else 
+            {
+                throw "Очередь пуста.";  // Генерируем исключение с сообщением
+            }
+        }
+        catch (const char* e) 
+        {
+            cout << "Произошла ошибка: " << e << endl;
+            throw;  // Повторно генерируем исключение
+        }
+    }
+
+    bool isEmpty() const override 
+    {
+        return front > rear;
+    }
+};
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+    Stack stack(7);
+    Queue queue(7);
+
+    try 
+    {
+        // Добавляем элементы в стек, для примера специально увеличим количество
+        for (int i = 1; i <= 8; i++) 
+        {  // Переполним стек
+            stack.insert(i);
+        }
+        cout << "Извлечение элементов из стека:" << endl;
+        while (!stack.isEmpty()) 
+        {
+            cout << stack.remove() << " ";
+        }
+        cout << endl;
+    }
+    catch (...) //универсальный catch
+    {
+        cout << "Обработка ошибки в основной функции." << endl;
+    }
+
+    try 
+    {
+        // Добавляем элементы в очередь
+        for (int i = 1; i <= 7; i++) 
+        {  // Переполним очередь
+            queue.insert(i);
+        }
+        cout << "Извлечение элементов из очереди:" << endl;
+        while (!queue.isEmpty()) 
+        {
+            cout << queue.remove() << " ";
+        }
+        cout << endl;
+    }
+    catch (...) //универсальный catch
+    {
+        cout << "Обработка ошибки в основной функции." << endl;
+    }
+    return 0;
+}
---
+++ ДЗ №9 до 18.09.23 Исключения и пространства имен/ДЗ №9_2 до 18.09.23 пространства имен.cpp
@@ -0,0 +1,90 @@
+﻿// ДЗ №9_2 до 18.09.23 пространства имен
+//
+
+//Задание 2. Создайте пространство имён для вычислений на координатной
+//плоскости, создайте там классы для точки и многоугольника. Создайте
+//другое пространство имён для классов - хранилищ и создайте там классы
+//для динамических массива и матрицы. В функции main() основного
+//файла, матрицу, которая хранила бы точки, и функцию, которая
+//превращала бы эту матрицу в многоугольник.
+
+#include <iostream>
+#include <cmath> // Для использования функции sqrt
+
+// Используем пространство имён std
+using namespace std;
+
+// Создаем пространство имён для вычислений на координатной плоскости
+namespace Coordinates 
+{
+    // Класс для представления точки на плоскости
+    class Point 
+    {
+    public:
+        double x, y; // Координаты точки
+
+        // Конструктор для инициализации координат точки
+        Point(double xCoord, double yCoord) : x(xCoord), y(yCoord) {}
+    };
+
+    // Класс для представления многоугольника на плоскости
+    class Polygon 
+    {
+    public:
+        // Конструктор для создания многоугольника с заданными вершинами
+        Polygon(const Point* vertices, int numVertices) : vertices(vertices), numVertices(numVertices)  { }
+
+        // Метод для вывода координат точек многоугольника
+        void printVertices() 
+        {
+            cout << "Вершины многоугольника:" << endl;
+            for (int i = 0; i < numVertices; i++) 
+            {
+                cout << "x: " << vertices[i].x << ", y: " << vertices[i].y << endl;
+            }
+        }
+
+    private:
+        const Point* vertices; // Массив вершин многоугольника
+        int numVertices;       // Количество вершин
+    };
+}
+
+// Создаем пространство имён для классов - хранилищ
+namespace Storage 
+{
+    // Класс для динамического массива
+    class DynamicArray 
+    {
+    public:
+        // Конструктор для создания динамического массива с заданным размером
+        DynamicArray(int size) { }
+    };
+
+    // Класс для матрицы
+    class Matrix 
+    {
+    public:
+        // Конструктор для создания матрицы с заданными размерами
+        Matrix(int rows, int cols) { }
+    };
+}
+
+int main() 
+{
+    setlocale(LC_ALL, "Rus");
+    // Создаем матрицу для хранения точек
+    Storage::Matrix pointMatrix(3, 2); // Например, 3 строки и 2 столбца для координат x и y
+
+    // Создаем многоугольник из точек в матрице
+    Coordinates::Point vertices[] = { Coordinates::Point(1.0, 2.0), Coordinates::Point(3.0, 4.0), Coordinates::Point(5.0, 6.0) };
+    Coordinates::Polygon polygon(vertices, 3);
+
+    // Выводим координаты точек многоугольника
+    polygon.printVertices();
+
+    return 0;
+}
+
+
+
